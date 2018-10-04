a6bf9682a42e6f53a7f503a46d614e632e1e9710
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2018-10-04 15:59:28 +0600

    Translate NotFound component

--- src/app/components/NotFound/index.tsx
+++ src/app/components/NotFound/index.tsx
@@ -1,12 +1,19 @@
 import * as React from 'react';
+import { FormattedMessage } from 'react-intl';
 
-export class NotFound extends React.Component {
+export namespace NotFound {
+  export interface Props {
+    messages: any
+  }
+}
+
+export class NotFound extends React.Component<NotFound.Props> {
   render() {
     return (
       <div className="text-center d-flex">
         <div className="not-found-content">
           <h1>404</h1>
-          <h3>Page not found</h3>
+          <h3><FormattedMessage id="pageNotFound" defaultMessage="Page not found" /></h3>
         </div>
       </div>
     );
