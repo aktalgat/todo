0c16cd3b29e9b2cc858001cd71dc314da40e5cd5
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2019-03-01 12:00:01 +0100

    Rename main component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
 import { Route, Switch } from 'react-router';
-import { App as StoneApp } from 'app/containers/App';
+import { App as TodoApp } from 'app/containers/App';
 import { hot } from 'react-hot-loader';
 
 export const App = hot(module)(() => (
   <Switch>
-    <Route path="/" component={StoneApp} />
+    <Route path="/" component={TodoApp} />
   </Switch>
 ));
